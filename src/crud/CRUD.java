6972e42ba7deb887bc798a293c0d86e5d800bd61
Gabriel Vianna <gabrielvianna495@gmail.com>
2025-03-11 10:29:22 -0300

    Criação do método read, que lê um objeto do arquivo binário

--- src/crud/CRUD.java
+++ src/crud/CRUD.java
@@ -2,7 +2,6 @@ package crud;
 
 import java.io.*;
 import produtos.*;
-import java.util.*;
 
 public class CRUD {
     
@@ -31,6 +30,37 @@ public class CRUD {
         e.printStackTrace();
     }
   }
+
+  public Jogo read(int id){
+
+    try(RandomAccessFile raf = new RandomAccessFile("games.db", "r")){
+   
+    raf.seek(4); // Move o ponteiro para o byte logo após o cabeçalho
+    //Vai fazer a leitura do arquivo até ele acabar
+    while(raf.getFilePointer() < raf.length()){
+      
+    if(raf.readByte() != '*'){
+        int tamRegistro = raf.readInt();
+
+        byte[] ba  = new byte[tamRegistro];
+        //Uso o método readFully porque eu sei o tamanho do que vai ser lido
+        raf.readFully(ba);
+
+        Jogo jogo = new Jogo();
+        jogo.fromByteArray(ba);
+
+        if(jogo.getId() == id){
+            return jogo;
+        }
+      }
+    }
+ }catch(IOException e){
+        e.printStackTrace();
+    }
+    return null;
+  }
+
+
 
   
 }
